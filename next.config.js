3e1a31a880310026ff2f7cc1cf209711a668d957
Reymart <utspantonia@gmail.com>
2023-04-08 05:16:29 +0800

    update settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = 'utshp.github.io'
+   assetPrefix = `/${repo}/`
+   basePath = `/${repo}`
+}
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     remotePatterns: [
       {
