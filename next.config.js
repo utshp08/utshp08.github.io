721600dd5d5aaee71428705591e13b0adf08413d
Reymart <utspantonia@gmail.com>
2023-04-08 05:47:41 +0800

    update settings

--- next.config.js
+++ next.config.js
@@ -1,31 +1,20 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = ''
-let basePath = '/'
+let assetPrefix = "utshp08.github.io";
+let basePath = "";
 
 if (isGithubActions) {
-  const repo = 'utshp08.github.io'
-   assetPrefix = `/${repo}/`
-   basePath = `/${repo}`
+  const repo = "utshp08.github.io";
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
-
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   assetPrefix: assetPrefix,
   basePath: basePath,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "mir-s3-cdn-cf.behance.net",
-        port: "",
-        pathname: "/user/**/**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
