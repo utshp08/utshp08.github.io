10c4908617c25b8a17c70767723725966307fd00
Reymart <utspantonia@gmail.com>
2023-04-11 01:00:03 +0800

    update heading

--- src/components/common/heading.tsx
+++ src/components/common/heading.tsx
@@ -3,7 +3,7 @@ import { AppProps } from 'next/app';
 
 const Heading = ({children}: any) => {
     return (
-        <h1 className='relative mb-10 xl:text-4xl lg:text-4xl md:text-4xl sm:text-2xl xs:text-2xl before:content-[""] before:block 
+        <h1 className='relative mb-10 text-4xl xl:text-4xl lg:text-4xl md:text-4xl sm:text-2xl xs:text-2xl before:content-[""] before:block 
             before:absolute before:top-[170%] before:left-[25%] before:w-[50%] before:h-[5px] before:bg-yellow-500 '>
             {children}
         </h1>
