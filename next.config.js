2361d9b4da01e4b5b32f69d36f20b47fb727c6d5
Reymart <utspantonia@gmail.com>
2023-04-10 21:23:41 +0800

    udpate script

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let assetPrefix = "";
-let basePath = "/";
+let basePath = "";
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`;
+  assetPrefix = `${repo}/`;
   basePath = `/${repo}`;
 }
 
